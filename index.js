ff9a8c307ea9a0f77b14ac34ad2454924ce8a383
geovane31 <geovane.rabelo10@gmail.com>
2024-08-18 17:17:51 -0300

    Projeto Final

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("teste")
+// Class heroi determinando o nome, idade e tipo
+class Heroi {
+    constructor(nome , idade, tipo){
+        this.nome = nome
+        this.idade = idade
+        this.tipo = tipo
+    }
+    //Função com estrutura de decisão para o tipo.
+    atacar(){
+        let ataque;
+        if (this.tipo === "mago"){
+            ataque = "Magia"
+        }
+        else if (this.tipo === "guerreiro"){
+            ataque = "Espada"
+        }           
+        else if (this.tipo === "monge"){
+            ataque = "Artes marciais"
+        }       
+        else if (this.tipo === "ninja"){
+            ataque = "Shuriken"
+        }    
+    console.log(`O ${this.tipo} ${this.nome} atacou usando ${ataque}!`)
+    }
+}
+
+let heroi1 = new Heroi ("Rei Arthur", 27, "guerreiro")
+heroi1.atacar()
